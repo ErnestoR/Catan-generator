0d4c347c1764ba5b4019207e1b790b0a97cf84e1
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2017-09-30 19:28:49 -0500

    added base grid layout

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -4,38 +4,38 @@ import './style.css';
 import Hexagon from './../hexagon';
 
 const Grid = () => {
-  const horizontalSize = 4;
-  const verticalSize = 4;
-
-
-  const renderColumns = (evenOdd) => {
-    let result = [];
-
-    for (let x = 1; x <= horizontalSize; x++) {
-      result.push(<Hexagon/>);
-    }
-
-    return result
-  };
-
-  const renderRows = () => {
-    let result = [];
-
-    for (let x = 1; x <= verticalSize; x++) {
-      result.push((
-        <div className="row">
-          {renderColumns()}
-        </div>
-      ));
-    }
-
-    return result
-  };
-
 
   return (
     <div id="grid" className="clear">
-      {renderRows()}
+      <div className="row">
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+      </div>
+      <div className="row">
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+      </div>
+      <div className="row">
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+      </div>
+      <div className="row">
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+      </div>
+      <div className="row">
+        <Hexagon/>
+        <Hexagon/>
+        <Hexagon/>
+      </div>
     </div>
   )
 };
