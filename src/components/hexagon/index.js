3d498c5c8a6374770c77509018fa03b148d3cfd5
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2017-09-30 20:16:11 -0500

    Added random hex backgrounds

--- src/components/hexagon/index.js
+++ src/components/hexagon/index.js
@@ -3,8 +3,15 @@ import React from "react";
 import './style.css';
 
 const hexagon = () => {
+  let randomNum = () => Math.floor(Math.random() * 1000);
+  let color =  ('00' + randomNum()).slice(-3) ;
+
+  const styles = {
+    backgroundColor : `#${color}`
+  };
+
   return (
-    <div className="hexagon">
+    <div className="hexagon" style={styles}>
     </div>
   )
 };
